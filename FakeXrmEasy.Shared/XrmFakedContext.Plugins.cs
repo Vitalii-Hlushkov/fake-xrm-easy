47940b2c8c7e110287eee73f65243c69ca37754c
Vitalii Hlushkov <vitalii.hlushkov@gmail.com>
2017-04-06 18:35:26 +0300

    Refactored and extended faked definition of the IPluginExecutionContext in compliance with CRM 2011-2016.

--- FakeXrmEasy.Shared/XrmFakedContext.Plugins.cs
+++ FakeXrmEasy.Shared/XrmFakedContext.Plugins.cs
@@ -38,34 +38,43 @@ namespace FakeXrmEasy
 
             var newUserId = Guid.NewGuid();
 
+            A.CallTo(() => context.BusinessUnitId).ReturnsLazily(() => ctx.BusinessUnitId == Guid.Empty ? Guid.NewGuid() : ctx.BusinessUnitId);
+            A.CallTo(() => context.CorrelationId).ReturnsLazily(() => ctx.CorrelationId == Guid.Empty ? Guid.NewGuid() : ctx.CorrelationId);
             A.CallTo(() => context.Depth).ReturnsLazily(() => ctx.Depth <= 0 ? 1 : ctx.Depth);
-            A.CallTo(() => context.IsExecutingOffline).ReturnsLazily(() => ctx.IsExecutingOffline);
+            A.CallTo(() => context.InitiatingUserId).ReturnsLazily(() => ctx.InitiatingUserId == Guid.Empty ? newUserId : ctx.InitiatingUserId);
             A.CallTo(() => context.InputParameters).ReturnsLazily(() => ctx.InputParameters);
-            A.CallTo(() => context.OutputParameters).ReturnsLazily(() => ctx.OutputParameters);
-            A.CallTo(() => context.PreEntityImages).ReturnsLazily(() => ctx.PreEntityImages);
-            A.CallTo(() => context.PostEntityImages).ReturnsLazily(() => ctx.PostEntityImages);
+            A.CallTo(() => context.IsExecutingOffline).ReturnsLazily(() => ctx.IsExecutingOffline);
+            A.CallTo(() => context.IsInTransaction).ReturnsLazily(() => ctx.IsInTransaction);
+            A.CallTo(() => context.IsOfflinePlayback).ReturnsLazily(() => ctx.IsOfflinePlayback);
+            A.CallTo(() => context.IsolationMode).ReturnsLazily(() => ctx.IsolationMode == 0 ? 1 : 2);
             A.CallTo(() => context.MessageName).ReturnsLazily(() => ctx.MessageName);
             A.CallTo(() => context.Mode).ReturnsLazily(() => ctx.Mode);
-            A.CallTo(() => context.OrganizationName).ReturnsLazily(() => ctx.OrganizationName);
-            A.CallTo(() => context.OrganizationId).ReturnsLazily(() => ctx.OrganizationId);
-            A.CallTo(() => context.InitiatingUserId).ReturnsLazily(() => ctx.InitiatingUserId == Guid.Empty ? newUserId : ctx.InitiatingUserId);
+            A.CallTo(() => context.OperationCreatedOn).ReturnsLazily(() => ctx.OperationCreatedOn == DateTime.MinValue ? DateTime.UtcNow : ctx.OperationCreatedOn);
+            A.CallTo(() => context.OperationId).ReturnsLazily(() => ctx.OperationId == Guid.Empty ? Guid.NewGuid() : ctx.OperationId);
+            A.CallTo(() => context.OrganizationId).ReturnsLazily(() => ctx.OrganizationId == Guid.Empty ? Guid.NewGuid() : ctx.OrganizationId);
+            A.CallTo(() => context.OrganizationName).ReturnsLazily(() => ctx.OrganizationName ?? "Default organization");
+            A.CallTo(() => context.OutputParameters).ReturnsLazily(() => ctx.OutputParameters);
+            A.CallTo(() => context.OwningExtension).ReturnsLazily(() => ctx.OwningExtension);
+            A.CallTo(() => context.PostEntityImages).ReturnsLazily(() => ctx.PostEntityImages);
+            A.CallTo(() => context.PreEntityImages).ReturnsLazily(() => ctx.PreEntityImages);
+            A.CallTo(() => context.PrimaryEntityId).ReturnsLazily(() => ctx.PrimaryEntityId);
+            A.CallTo(() => context.PrimaryEntityName).ReturnsLazily(() => ctx.PrimaryEntityName);
+            A.CallTo(() => context.RequestId).ReturnsLazily(() => ctx.RequestId == Guid.Empty ? Guid.NewGuid() : ctx.RequestId);
+            A.CallTo(() => context.SecondaryEntityName).ReturnsLazily(() => ctx.SecondaryEntityName);
+            A.CallTo(() => context.SharedVariables).ReturnsLazily(() => ctx.SharedVariables);
             A.CallTo(() => context.UserId).ReturnsLazily(() => ctx.UserId == Guid.Empty ? newUserId : ctx.UserId);
             A.CallTo(() => context.ParentContext).ReturnsLazily(() => ctx.ParentContext);
             A.CallTo(() => context.Stage).ReturnsLazily(() => ctx.Stage);
-            A.CallTo(() => context.PrimaryEntityName).ReturnsLazily(() => ctx.PrimaryEntityName);
-            A.CallTo(() => context.SecondaryEntityName).ReturnsLazily(() => ctx.SecondaryEntityName);
-            A.CallTo(() => context.SharedVariables).ReturnsLazily(() => ctx.SharedVariables);
-
             //Create message will pass an Entity as the target but this is not always true
             //For instance, a Delete request will receive an EntityReference
-            if (ctx.InputParameters != null &&
-                ctx.InputParameters.ContainsKey("Target") &&
-                ctx.InputParameters["Target"] is Entity)
+            if (ctx.InputParameters == null || !ctx.InputParameters.ContainsKey("Target") ||
+                !(ctx.InputParameters["Target"] is Entity))
             {
-                var target = ctx.InputParameters["Target"] as Entity;
-                A.CallTo(() => context.PrimaryEntityId).ReturnsLazily(() => target.Id);
-                A.CallTo(() => context.PrimaryEntityName).ReturnsLazily(() => target.LogicalName);
+                return context;
             }
+            var target = (Entity) ctx.InputParameters["Target"];
+            A.CallTo(() => context.PrimaryEntityId).ReturnsLazily(() => target.Id);
+            A.CallTo(() => context.PrimaryEntityName).ReturnsLazily(() => target.LogicalName);
 
             return context;
         }
